45928fb393c58a50471ef640334c3cf28dc2855d
Karan Sharma <karansharma1295@gmail.com>
2015-10-29 12:52:50 +0530

    DH1

--- MessJSON/data.py
+++ MessJSON/data.py
@@ -2,11 +2,17 @@
 # coding: utf-8
 
 import pandas as pd
-df = pd.read_excel('DH2.xlsx')
-df=df.dropna(how='any')
-z=df.iloc[:,(1,2,3,4,5,6,7)]
-z.columns=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-z.to_json("week.json")
+dh1 = pd.read_excel('DH1.xlsx')
+dh2 = pd.read_excel('DH2.xlsx')
+dh1=dh1.dropna(how='any')
+dh2=dh2.dropna(how='any')
+dh1z=dh1.iloc[:,(1,2,3,4,5,6,7)]
+dh2z=dh2.iloc[:,(1,2,3,4,5,6,7)]
+dh1z.columns=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+dh2z.columns=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+dh1z.to_json("DH1.json")
+dh2z.to_json("DH2.json")
+
 
 
 
